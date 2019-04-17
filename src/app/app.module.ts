2480b1e600f8abd794ce5dd1934e6ae1f2571eae
Lorenz Graf <lorenz.graaf@gmail.com>
2019-04-17 21:26:16 +0200

    glitch is now welcome screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {PersonalInfoComponent} from './components/personal-info/personal-info.component';
 import {LinkFooterComponent} from './components/link-footer/link-footer.component';
+import {HomePageModule} from './pages/home/home.module';
+import {HackerConComponent} from './components/hacker-con/hacker-con.component';
+import {ScrollingMouseComponent} from './components/scrolling-mouse/scrolling-mouse.component';
 
 @NgModule({
-    declarations: [AppComponent, PersonalInfoComponent, LinkFooterComponent],
+    declarations: [AppComponent, PersonalInfoComponent, LinkFooterComponent, HackerConComponent, ScrollingMouseComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HomePageModule
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,21 +6,22 @@ import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 import {FancyCanvasComponent} from '../../components/fancy-canvas/fancy-canvas.component';
-import {ScrollingMouseComponent} from '../../components/scrolling-mouse/scrolling-mouse.component';
-import {HackerConComponent} from '../../components/hacker-con/hacker-con.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
-  ],
-  declarations: [HomePage, FancyCanvasComponent, ScrollingMouseComponent, HackerConComponent]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: HomePage
+            }
+        ])
+    ],
+    exports: [
+
+    ],
+    declarations: [HomePage, FancyCanvasComponent]
 })
 export class HomePageModule {}
