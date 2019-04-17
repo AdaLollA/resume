21ee3623c1a2980a9098b4f64d4c042a3a85e3c0
Lorenz Graf <lorenz.graaf@gmail.com>
2019-04-17 20:52:23 +0200

    hackercon init finished

--- src/app/components/hacker-con/hacker-con.component.ts
+++ src/app/components/hacker-con/hacker-con.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-hacker-con',
   templateUrl: './hacker-con.component.html',
   styleUrls: ['./hacker-con.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HackerConComponent implements OnInit {
 
