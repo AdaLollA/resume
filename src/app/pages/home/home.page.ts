6d303995cf4d911b11b7e1199757c165216a621f
Lorenz Graf <lorenz.graaf@gmail.com>
2019-04-19 18:11:47 +0200

    menu is active listener in home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {SwipeScrollDirection, SwipeScrollListener, SwipeScrollListenService} from '../../services/swipe-scroll-listen.service';
 import {Router} from '@angular/router';
+import {MenuController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    public showMenu: boolean;
+
+    constructor(public menuCtrl: MenuController) {
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.menuCtrl.get().then((menu) => {
+            menu.isActive().then((active) => {
+                this.showMenu = active;
+                console.log(this.showMenu);
+            });
+        });
+    }
 }
