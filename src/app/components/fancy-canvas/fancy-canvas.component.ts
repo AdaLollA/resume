916048c17560ba99617596024b0cb5ccf9dab1c2
Unknown <lorenz.graaf@gmail.com>
2019-04-23 00:25:24 +0200

    flashing dots

--- src/app/components/fancy-canvas/fancy-canvas.component.ts
+++ src/app/components/fancy-canvas/fancy-canvas.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {CanvasSpace, Num} from 'pts';
+import {CanvasSpace, Num, Pt} from 'pts';
+
+export interface IFlyer {
+    color: string
+    position: number[]
+    direction?
+}
 
 @Component({
     selector: 'app-fancy-canvas',
@@ -9,18 +15,65 @@ import {CanvasSpace, Num} from 'pts';
 export class FancyCanvasComponent implements OnInit {
 
     ngOnInit(): void {
+        // variables
+        const count = 150;
+        const spread = 0.8;
+        const colors: string[] = [
+            '#1aff1c', '#ff2f31', '#2d75ff'
+        ];
+
+        // create
         let space = new CanvasSpace('#beauty');
-        space.setup({ bgcolor: "#181818" });
+        space.setup({bgcolor: '#181818'});
         let form = space.getForm();
+        let e: IFlyer[] = [];
 
-        // space.add( () => form.point( space.pointer, 10 ) );
+        for (let i = 0; i < count; i++) {
+            const flyer: IFlyer = {
+                color: colors[this.numberBetween(0, 2)],
+                position: [0, 0]
+            };
+            e.push(flyer);
+        }
 
-        space.add( (time, ftime) => {
-            let radius = Num.cycle( (time%1000)/1000 ) * 20;
-            form.fill("#09f").point( space.pointer, radius, "circle" );
+        // animation
+        space.add((time, ftime) => {
+            const cx = space.center.x;
+            const cy = space.center.y;
+
+            e.forEach((el) => {
+               el.position = [
+                   cx + (this.numberBetween(0, cx*spread*2) - cx*spread),
+                   cy + (this.numberBetween(0, cy*spread*2) - cy*spread)
+               ]
+            });
+
+
+            // form.stroke('#42e', 5).fill('#42e').point(space.center, 3, 'circle');
+
+            e.forEach((el) => {
+                form.stroke(el.color, 5).fill(el.color).point(el.position, 3, 'circle');
+            });
+
+            // space.pointer stores the last mouse or touch position
+            let m = space.pointer;
+
+            // drawing
+            /*
+            form.strokeOnly("#123", 5).line( [new Pt( m.x, 0), m, new Pt( 0, m.y)] );
+            form.stroke("#42e").line( [new Pt(0,0), m] );
+            form.stroke("#fff", 5).fill("#42e").point( m, 10, "circle");
+            form.fill("#123").font(14, "bold").text( m.$add(20, 5), m.toString() );
+            */
+
         });
 
+        // play
         space.play().bindMouse().bindTouch();
+    }
+
+    private numberBetween(min: number, max: number) {
+        return Math.floor(Math.random() * max) + min;
     }
 
 }
