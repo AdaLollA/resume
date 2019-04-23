cb8632c34ee00be0d26520e20e533ab42c4cdfb1
Lorenz Graf <lorenz.graaf@gmail.com>
2019-04-23 16:27:16 +0200

    hover

--- src/app/components/fancy-canvas/fancy-canvas.component.ts
+++ src/app/components/fancy-canvas/fancy-canvas.component.ts
@@ -5,6 +5,7 @@ export interface IFlyer {
     color?: string
     offset?: number
     radius?: number
+    opacity: number
 }
 
 @Component({
@@ -17,20 +18,23 @@ export class FancyCanvasComponent implements OnInit {
 
     ngOnInit(): void {
         // variables
-        const count = 100;
-        const lineColor: string = 'rgba(141,141,141,0.20)';
+        const angle: number = Const.two_pi - Const.quarter_pi * 3 / 2;
+        const lineOpacity = 0.20;
+        const count = 50;
         const colors: string[] = [
             '#1aff1c', '#ff2f31', '#2d75ff'
         ];
         let tempo = new Tempo(10);
+        const lineColor: string = 'rgba(141,141,141,';
 
         // create
+        this.init = false;
         let space = new CanvasSpace('#beauty');
         space.setup({bgcolor: '#252934'});
         let form = space.getForm();
         let e: IFlyer[] = [];
         for (let i = 0; i < count; i++) {
-            e.push({});
+            e.push({opacity: lineOpacity});
         }
 
         // setup
@@ -40,25 +44,34 @@ export class FancyCanvasComponent implements OnInit {
             if (!this.init) {
                 // init
                 e.forEach((el) => {
-                    el.color = colors[this.numberBetween(0, colors.length)];
-                    el.offset = this.numberBetween(0, Const.two_pi*100) / 100;
-                    el.radius = this.numberBetween(0, this.pythagorasC(space.height, space.width) / 2);
+                    el.color = colors[FancyCanvasComponent.numberBetween(0, colors.length)];
+                    el.offset = FancyCanvasComponent.numberBetween(0, Const.two_pi * 100) / 100;
+                    el.radius = FancyCanvasComponent.numberBetween(0, space.width / 2);
                 });
                 this.init = true;
             }
         });
 
         // animate
-        // todo tempo
         tempo.every(20).progress((count: number, t: number, ms: number, start: boolean) => {
             e.forEach((el) => {
                 // dots
                 let dotLn = Line.fromAngle(space.center, Const.two_pi * t - Const.half_pi + el.offset, el.radius);
                 form.fillOnly(el.color).point(dotLn.p2, 1, 'circle');
 
+                // opacity
+                el.opacity = el.opacity > lineOpacity ? el.opacity - 0.05 : el.opacity;
+
                 // lines
-                let ln = Line.fromAngle(dotLn.p2, Const.two_pi - Const.quarter_pi * 3 / 2, this.pythagorasC(space.height, space.width) * 2);
-                form.strokeOnly( lineColor, 1, "round", "round" ).line( ln );
+                let ln = Line.fromAngle(dotLn.p2, angle, space.height + space.width);
+                form.strokeOnly(lineColor + el.opacity + ')', 1, 'round', 'round').line(ln);
+
+                // hover
+                form.point(space.pointer, 10); // todo this is cursor debug - seems to be offset by menu
+                let distance: number = Line.perpendicularFromPt(ln, space.pointer, true).magnitude();
+                if (distance < 10) {
+                    el.opacity = 1;
+                }
             });
         }, 0);
         space.add(tempo as any);
@@ -67,16 +80,12 @@ export class FancyCanvasComponent implements OnInit {
         space.play().bindMouse().bindTouch();
     }
 
-    private numberBetween(min: number, max: number) {
+    private static numberBetween(min: number, max: number) {
         return Math.floor(Math.random() * max) + min;
     }
 
-    private distanceBetweenPoints(pointA: number[], pointB: number[]): number {
-        return Math.sqrt(Math.pow((pointA[0] - pointB[0]), 2) + Math.pow((pointA[1] - pointB[1]), 2));
-    }
-
-    private pythagorasC(a: number, b: number): number {
-        return Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+    private static radToDeg(rad: number) : number {
+        return rad * 180 / Math.PI;
     }
 
 }
