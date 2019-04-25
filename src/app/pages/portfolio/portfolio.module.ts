f9d8e30c95227e5b7c2f013ce17f19145c19667c
Lorenz Graf <lorenz.graaf@gmail.com>
2019-04-25 10:36:42 +0200

    toggle button init

--- src/app/pages/portfolio/portfolio.module.ts
+++ src/app/pages/portfolio/portfolio.module.ts
@@ -1,27 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { PortfolioPage } from './portfolio.page';
+import {PortfolioPage} from './portfolio.page';
 import {ProjectCardComponent} from '../../components/project-card/project-card.component';
+import {MatButtonToggleModule} from '@angular/material';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PortfolioPage
-  }
+    {
+        path: '',
+        component: PortfolioPage
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [PortfolioPage, ProjectCardComponent]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes),
+        MatButtonToggleModule
+    ],
+    declarations: [PortfolioPage, ProjectCardComponent]
 })
-export class PortfolioPageModule {}
+export class PortfolioPageModule {
+}
