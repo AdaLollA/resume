9cf6f7fefde6481d245c5b9a635a66c676a531a3
Lorenz Graf <lorenz.graaf@gmail.com>
2019-05-09 10:21:49 +0200

    environment setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDW4D9ETctlksuwBEqkB0ur9SohPle_ViI",
+    authDomain: "resume-73460.firebaseapp.com",
+    databaseURL: "https://resume-73460.firebaseio.com",
+    projectId: "resume-73460",
+    storageBucket: "resume-73460.appspot.com",
+    messagingSenderId: "516118575379",
+    appId: "1:516118575379:web:db4b4e977ac5877d"
+  }
 };
 
 /*
