34751182d1cd3f2a5bb1a95c53b29332d51b6b49
Lorenz Graf <lorenz.graaf@gmail.com>
2019-07-04 19:37:52 +0200

    sizing progress

--- src/app/pages/skills/skills.page.ts
+++ src/app/pages/skills/skills.page.ts
@@ -1,9 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {ISkill} from '../../components/skill/skill.component';
 import {MenuStateService} from '../../services/menu-state.service';
 import {Chart} from 'chart.js';
 import {Observable} from 'rxjs';
-import {Router} from '@angular/router';
 import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
@@ -82,30 +80,36 @@ export class SkillsPage implements OnInit {
                     data: this.skills[1].data,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 132, 0.2)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 132, 1)'
                     ],
                     borderWidth: 1
                 }]
             },
             options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
+                legend: {
+                    display: false
                 }
             }
         });
@@ -146,7 +150,6 @@ export class SkillsPage implements OnInit {
                         }
                     }]
                 },
-                animation: {},
                 legend: {
                     display: false
                 }
