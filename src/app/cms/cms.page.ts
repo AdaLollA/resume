82bee7eedb6da3904275cd29256993f682677f9c
Lorenz Graf <lorenz.graaf@gmail.com>
2019-12-21 16:52:32 +0100

    init page

---
+++ src/app/cms/cms.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cms',
+  templateUrl: './cms.page.html',
+  styleUrls: ['./cms.page.scss'],
+})
+export class CmsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
