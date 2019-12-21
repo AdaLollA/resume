274b02388fb8a1c78a43df2ecda21acfad4ca745
Lorenz Graf <lorenz.graaf@gmail.com>
2019-12-21 17:03:15 +0100

    auth service init

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  // https://fireship.io/lessons/angularfire-google-oauth/
+  constructor() { }
+}
