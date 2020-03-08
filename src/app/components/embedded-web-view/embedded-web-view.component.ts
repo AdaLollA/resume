0f7d90646825a56c12d20b26e1ab6a7c2b00bbc3
Unknown <lorenz.graaf@gmail.com>
2020-03-08 15:11:20 +0100

    iframe init

--- src/app/components/embedded-web-view/embedded-web-view.component.ts
+++ src/app/components/embedded-web-view/embedded-web-view.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-embedded-web-view',
   templateUrl: './embedded-web-view.component.html',
   styleUrls: ['./embedded-web-view.component.scss'],
 })
-export class EmbeddedWebViewComponent implements OnInit {
+export class EmbeddedWebViewComponent {
+  url = 'https://dronify-5949c.firebaseapp.com/';
 
-  constructor() { }
+  constructor(public sanitizer: DomSanitizer) { }
 
-  ngOnInit() {}
+  /**
+   * Loads the provided url into the embedded web view.
+   *
+   * @param url The url that is to be loaded.
+   */
+  public load(url: string) {
+    this.url = url;
+  }
 
 }
