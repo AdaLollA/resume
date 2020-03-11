77480a6d913f49fc866b408d77d438b081f99ff6
Unknown <lorenz.graaf@gmail.com>
2020-03-11 10:35:44 +0100

    person edit image drag and drop works

--- src/app/cms/cms.module.ts
+++ src/app/cms/cms.module.ts
@@ -1,34 +1,37 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { CmsPage } from './cms.page';
+import {CmsPage} from './cms.page';
 import {CmsElementComponent} from './cms-element/cms-element.component';
 import {TimelineVisualizerComponent} from './timeline-visualizer/timeline-visualizer.component';
 import {TimelineEditorComponent} from './modals/timeline-editor/timeline-editor.component';
 import {TeamEditorComponent} from './modals/team-editor/team-editor.component';
+import {NgxFileHelpersModule} from 'ngx-file-helpers';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: CmsPage
-  }
+    {
+        path: '',
+        component: CmsPage
+    }
 ];
 
 @NgModule({
-  entryComponents:[
-    TimelineEditorComponent,
-    TeamEditorComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [CmsPage, CmsElementComponent, TimelineVisualizerComponent, TimelineEditorComponent, TeamEditorComponent]
+    entryComponents: [
+        TimelineEditorComponent,
+        TeamEditorComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes),
+        NgxFileHelpersModule
+    ],
+    declarations: [CmsPage, CmsElementComponent, TimelineVisualizerComponent, TimelineEditorComponent, TeamEditorComponent]
 })
-export class CmsPageModule {}
+export class CmsPageModule {
+}
--- src/app/cms/modals/team-editor/team-editor.component.ts
+++ src/app/cms/modals/team-editor/team-editor.component.ts
@@ -3,78 +3,78 @@ import {ITimelineObject} from '../../../components/timeline/timeline.component';
 import {AuthService, CmsType} from '../../../services/auth.service';
 import {ModalController, NavParams} from '@ionic/angular';
 import {ITeamMember} from '../../../pages/team/team.page';
+import {ReadFile} from 'ngx-file-helpers';
 
 @Component({
-  selector: 'app-team-editor',
-  templateUrl: './team-editor.component.html',
-  styleUrls: ['./team-editor.component.scss'],
+    selector: 'app-team-editor',
+    templateUrl: './team-editor.component.html',
+    styleUrls: ['./team-editor.component.scss'],
 })
 export class TeamEditorComponent implements OnInit {
 
-  @Input() data: ITeamMember;
-  public modifiedData: ITeamMember;
+    @Input() data: ITeamMember;
+    public modifiedData: ITeamMember;
 
-  public changes: boolean = false;
-  public now: Date;
-  private type = CmsType.TEAM;
+    public changes: boolean = false;
+    public now: Date;
+    private type = CmsType.TEAM;
 
-  constructor(public modalCtrl: ModalController, private auth: AuthService) {
-  }
-
-  ngOnInit(): void {
-    if (this.data) {
-      // edit existing data set
-      // clone modifiable data so we can react to changes
-      this.modifiedData = Object.assign({}, this.data);
-    } else {
-      // create new data set
-      this.modifiedData = {
-        img: '',
-        name: '',
-        profession: '',
-        index: 0
-      };
+    constructor(public modalCtrl: ModalController, private auth: AuthService) {
     }
-  }
 
-  save() {
-    if (this.data) {
-      // update existing data set
-      this.auth.update(this.modifiedData, this.type).then(
-          (res) => {
-            // no errors
-          },
-          (err) => {
-            console.error(err);
-          });
-    } else {
-      // create new data set
-      this.auth.create(this.modifiedData, this.type);
+    ngOnInit(): void {
+        if (this.data) {
+            // edit existing data set
+            // clone modifiable data so we can react to changes
+            this.modifiedData = Object.assign({}, this.data);
+        } else {
+            // create new data set
+            this.modifiedData = {
+                img: '',
+                name: '',
+                profession: '',
+                index: 0
+            };
+        }
     }
-    this.dismiss();
-  }
 
-  checkChanges() {
-    if (this.modifiedData.img != '' && this.modifiedData.name != '' && this.modifiedData.profession != '' && this.modifiedData.index != undefined) {
-      // all fields contain data
-      if (this.modifiedData != this.data) {
-        // fields have changed from previous data
-        this.changes = true;
-        return;
-      }
+    save() {
+        if (this.data) {
+            // update existing data set
+            this.auth.update(this.modifiedData, this.type).then(
+                (res) => {
+                    // no errors
+                },
+                (err) => {
+                    console.error(err);
+                });
+        } else {
+            // create new data set
+            this.auth.create(this.modifiedData, this.type);
+        }
+        this.dismiss();
     }
-    this.changes = false;
-  }
 
-  dismiss() {
-    this.modalCtrl.dismiss({
-      'dismissed': true
-    });
-  }
+    checkChanges() {
+        if (this.modifiedData.img != '' && this.modifiedData.name != '' && this.modifiedData.profession != '' && this.modifiedData.index != undefined) {
+            // all fields contain data
+            if (this.modifiedData != this.data) {
+                // fields have changed from previous data
+                this.changes = true;
+                return;
+            }
+        }
+        this.changes = false;
+    }
 
-  delete() {
-    this.auth.delete(this.data.id, this.type);
-    this.dismiss();
-  }
+    dismiss() {
+        this.modalCtrl.dismiss({
+            'dismissed': true
+        });
+    }
 
+    delete() {
+        this.auth.delete(this.data.id, this.type);
+        this.dismiss();
+    }
 }
